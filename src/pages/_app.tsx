4d5b827e93505703b9b7f45106bdff2dbd9424a0
Mayro <deehmyller11@gmail.com>
2022-09-19 22:57:42 -0300

    tarefa: adiciona conteúdo e estilo ao App arquivo de entrada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import Image from 'next/future/image'
 
-export default MyApp
+import { globalStyle } from '../styles/global'
+
+import * as S from '../styles/pages/app'
+
+import logoSVG from '../assets/logo.svg'
+
+globalStyle()
+
+export default function App({ Component, pageProps }) {
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src={logoSVG} alt="" />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
+  )
+}
