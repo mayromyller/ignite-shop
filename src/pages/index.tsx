26bc0ac3b52679455a85870e4ec919a1dbc5fd22
Mayro <deehmyller11@gmail.com>
2022-09-19 22:56:50 -0300

    tarefa: adiciona conteúdo e estilo a home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
+import Image from 'next/future/image'
+
+import Shirt1 from '../assets/shirts/1.png'
+import Shirt2 from '../assets/shirts/2.png'
+import Shirt3 from '../assets/shirts/3.png'
+
+import * as S from '../styles/pages/home'
+
 export default function Home() {
   return (
-   <h1>Ignite Shop</h1>
+    <S.Container>
+      <S.Product>
+        <Image src={Shirt1} width={520} height={480} alt="" />
+
+        <S.Footer>
+          <strong>Camiseta Premier League</strong>
+          <span>R$ 79,90</span>
+        </S.Footer>
+      </S.Product>
+
+      <S.Product>
+        <Image src={Shirt2} width={520} height={480} alt="" />
+
+        <S.Footer>
+          <strong>Camiseta Premier League</strong>
+          <span>R$ 79,90</span>
+        </S.Footer>
+      </S.Product>
+    </S.Container>
   )
 }
